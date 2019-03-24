b168717d52dd39d01633983e9f17405ebecacf83
mingz <305603665@qq.com>
2019-03-24 16:35:42 +0800

    tower

---
+++ TestTowerDefense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public TurretData laserTurretData;
+    public TurretData missileTurretData;
+    public TurretData standardTurretData;
+
+
+
+    private TurretData selectedTurretData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void OnLaserSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = laserTurretData;
+        }
+
+    }
+    public void OnMissileSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = missileTurretData;
+        }
+
+    }
+    public void OnStandardSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = standardTurretData;
+        }
+
+    }
+
+
+
+}
